b73955cfb14a42a545a6275537586fb270b7885c
Tito Luyo Murata <tluyo@hotmail.com>
2025-11-13 19:27:34 -0500

    temp

--- cabinasensor/esp32s3-blink/src/main.cpp
+++ cabinasensor/esp32s3-blink/src/main.cpp
@@ -1,22 +1,52 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-#define LED_PIN 47
+#define PIXEL_PIN 48
+#define NUM_PIXELS 1
+#define LED_BRIGHTNESS 32
+#define COLOR_SWITCH_DELAY_MS 1000
+
+static CRGB pixels[NUM_PIXELS];
+static const CRGB kColorSequence[] = {
+    CRGB::Red,
+    CRGB::Green,
+    CRGB::Blue,
+    CRGB::White
+};
+static const char *const kColorNames[] = {
+    "red",
+    "green",
+    "blue",
+    "white"
+};
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+    while (!Serial && millis() < 2000) {
+        delay(10);
+    }
 
-  Serial.println("ESP32-S3 Supermini Blink Started!");
-  Serial.print("LED Pin: ");
-  Serial.println(LED_PIN);
+    FastLED.addLeds<NEOPIXEL, PIXEL_PIN>(pixels, NUM_PIXELS);
+    FastLED.setBrightness(LED_BRIGHTNESS);
+
+    Serial.println("ESP32-S3 Supermini RGB demo ready");
+    Serial.print("RGB data pin: ");
+    Serial.println(PIXEL_PIN);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  Serial.println("LED ON");
-  delay(1000);
+    pixels[0] = CRGB::Red; FastLED.show(); delay(1000);
+    pixels[0] = CRGB::Green; FastLED.show(); delay(1000);
+    pixels[0] = CRGB::Blue; FastLED.show(); delay(1000);
 
-  digitalWrite(LED_PIN, LOW);
-  Serial.println("LED OFF");
-  delay(1000);
+    static size_t colorIndex = 0;
+
+    pixels[0] = kColorSequence[colorIndex];
+    FastLED.show();
+
+    Serial.print("LED color: ");
+    Serial.println(kColorNames[colorIndex]);
+
+    colorIndex = (colorIndex + 1) % (sizeof(kColorSequence) / sizeof(kColorSequence[0]));
+    delay(COLOR_SWITCH_DELAY_MS);
 }
